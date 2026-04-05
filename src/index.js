31e03f316edd3879ef3463fa19060e6e5a25e006
Prajwal-1703 <atharep1703@gmail.com>
2026-04-05 13:05:54 +0530

    Database connection

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//require('dotenv').config({path: "./.env" });
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: "./.env"
+});
+connectDB(); 
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+;( async () =>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log("Connected to MongoDB");
+        app.on("error", (error) => {
+            console.error("Server error:", error);
+            throw error
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+        throw error;
+    }
+}) ();
+
+*/
